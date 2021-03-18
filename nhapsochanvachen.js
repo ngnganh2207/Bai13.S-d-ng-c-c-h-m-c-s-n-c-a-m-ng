2b81fa510b3121c68f6b5a7152f37d8223740ba4
ngnganh2207 <ngocanhnguyen2207@gmail.com>
2021-03-18 13:18:50 +0700

    18.3.21

---
+++ nhapsochanvachen.js
@@ -0,0 +1,23 @@
+/*const num=window.prompt();
+const str=num.toString();//không ép kiểu sang chuỗi vẫn được vì nhập vào prompt đã là chuỗi rồi
+const result=[str[0]];//chưa hiểu [[]]
+for(let x=1;x<str.length;x++){
+    if((str[x-1]%2===0)&&(str[x]%2===0)){
+        result.push('-',str[x]);
+    }
+    else{
+        result.push(str[x]);
+    }
+}
+console.log(result.join(''));*/
+/*const num=window.prompt();
+const result=[num[0]];//chưa hiểu [[]]
+for(let x=1;x<num.length;x++){
+    if((num[x-1]%2===0)&&(num[x]%2===0)){
+        result.push('-',num[x]);
+    }
+    else{
+        result.push(num[x]);
+    }
+}
+console.log(result.join(''));*/
---
+++ nhapvaochuoi_vachuyenchuthuongsanghoa.js
@@ -0,0 +1,14 @@
+var str=prompt();
+var UPPER='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var LOWER='abcdefghijklmnopqrstuvwxyz';
+var result=[];
+for (var x=0;x<str.length;x++){
+    if(UPPER.indexOf(str[x]) == -1){
+        result.push(str[x].toUpperCase());
+    }else if(LOWER.indexOf(str[x]) == -1){
+        result.push(str[x].toLowerCase());
+    } else{
+        result.push(str[x]);
+    }
+}
+console.log(result.join(''));
